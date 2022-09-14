64a9c49c3e4bdb99271a212c836bab1b3aff35ad
Kavindu Sanjula Dasanayake <kavindusanjula.1@gmail.com>
2022-09-14 16:40:54 +0530

    Upgrade AssetManager😎
    Add texture holders and shader holders

--- RaavanaEngine/src/RE/AssetManager/AssetManager.cpp
+++ RaavanaEngine/src/RE/AssetManager/AssetManager.cpp
@@ -1,6 +1,8 @@
 #include <repch.h>
 #include "AssetManager.h"
 
+#include "Log.h"
+
 namespace RE {
 
 	AssetManager* AssetManager::INSTANCE = nullptr;
@@ -17,6 +19,28 @@ namespace RE {
 	uint32_t AssetManager::GenerateID()
 	{
 		return m_Available_ID++;
+	}
+
+	Ref<Texture> AssetManager::Create_Texture(const std::string& filename)
+	{
+		if (m_Textures.find(filename) != m_Textures.end()) {
+			RE_CORE_INFO("Texture alrady created!");
+			return m_Textures[filename];
+		}
+
+		m_Textures[filename] = Texture::Create(filename);
+		return m_Textures[filename];
+	}
+
+	Ref<Shader> AssetManager::Create_Shader(const std::string& filename)
+	{
+		if (m_Shaders.find(filename) != m_Shaders.end()) {
+			RE_CORE_INFO("Shader alrady created!");
+			return m_Shaders[filename];
+		}
+
+		m_Shaders[filename] = Shader::Create(filename);
+		return m_Shaders[filename];
 	}
 
 }
--- RaavanaEngine/src/RE/AssetManager/AssetManager.h
+++ RaavanaEngine/src/RE/AssetManager/AssetManager.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "RE/core.h"
+#include "RE/Renderer/Texture.h"
+#include "RE/Renderer/Shader.h"
+
+#include <unordered_map>
 
 namespace RE {
 
@@ -10,16 +14,24 @@ namespace RE {
 		AssetManager();
 		~AssetManager();
 
-	public:
+	private:
 		uint32_t GenerateID();
+		Ref<Texture> Create_Texture(const std::string& filename);
+		Ref<Shader> Create_Shader(const std::string& filename);
+
 
 	public:
 		static AssetManager* INSTANCE;
 		inline static uint32_t GetUniqueID() { return AssetManager::INSTANCE->GenerateID(); }
 
+		inline static Ref<Texture> CreateTexture(const std::string& filename = NO_TEXTURE) { return AssetManager::INSTANCE->Create_Texture(filename); }
+		inline static Ref<Shader> CreateShader(const std::string& filename) { return AssetManager::INSTANCE->Create_Shader(filename); }
+
 
 	private:
 		uint32_t m_Available_ID = 0;
+		std::unordered_map<std::string, Ref<Texture>> m_Textures;
+		std::unordered_map<std::string, Ref<Shader>> m_Shaders;
 
 
 	};
